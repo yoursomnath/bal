5dd906ab1128a7899758c38a948076e51feed734
Somnath Kumbhakar <97022038+yoursomnath@users.noreply.github.com>
2024-01-07 00:00:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let c = document.getElementById("canvas");
+
+let ctx = c.getContext("2d");
+let cWidth = window.innerWidth;
+let cHeight = window.innerHeight;
+c.style.width = `${cWidth}px`;
+c.style.height = `${cHeight}px`;
+c.style.height = window.innerHeight;
+c.style.background = "black";
